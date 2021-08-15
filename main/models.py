b463c700b6dd71ea4939b42c4a205674e277721a
Simanchal Pattanayak <83402452+SPdeveloper-19@users.noreply.github.com>
2021-08-15 20:34:34 +0530

    Update models.py

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ def upload_post_to(instance,filename):
 	return f'post_picture/{instance.user.username}/{filename}'
 
 class Post(models.Model):
+	id = models.BigAutoField(primary_key=True)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	text = models.TextField()
 	picture = models.ImageField(null=True, upload_to = upload_post_to,)
@@ -15,22 +16,26 @@ class Post(models.Model):
 
 
 class Like(models.Model):
+	id = models.BigAutoField(primary_key=True)
 	post = models.ForeignKey(Post, on_delete=models.CASCADE)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	time = models.DateTimeField(auto_now_add=True)
 
 class Dislike(models.Model):
+	id = models.BigAutoField(primary_key=True)
 	post = models.ForeignKey(Post, on_delete=models.CASCADE)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	time = models.DateTimeField(auto_now_add=True)	
 
 class Comment(models.Model):
+	id = models.BigAutoField(primary_key=True)
 	post = models.ForeignKey(Post, on_delete=models.CASCADE)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	time = models.DateTimeField(auto_now_add=True)
 	comm = models.TextField()
 
 class SubComment(models.Model):
+	id = models.BigAutoField(primary_key=True)
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	time = models.DateTimeField(auto_now_add=True)
 	comm = models.TextField()
